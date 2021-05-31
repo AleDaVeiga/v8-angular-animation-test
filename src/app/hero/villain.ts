125fabc7b078a30d210f8bbd28b4088ec925ca35
AleDaVeiga <alessandrodaveiga@gmail.com>
2021-05-31 17:48:27 -0300

    9 files deleted

--- src/app/hero/villain.ts
+++
@@ -1,8 +0,0 @@
-export class Villain {
-  // this is our data model. the "message" and "messageState" properties are used in the "banner" demo
-  constructor(
-    public name: string,
-    public message = '',
-    public messageState = 'hidden'
-  ) {}
-}
